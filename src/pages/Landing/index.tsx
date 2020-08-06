167a1a56d04963fff1d2e40748d8a688ba36cb06
alvesph <alvesph1@gmail.com>
2020-08-06 15:08:54 -0400

    Update Link para react-router-dom

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -28,15 +29,15 @@ function Landing() {
                 className="hero-image"
                 />
                 <div className="buttons-container">
-                    <a href="/study" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estude"/>
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href="/give-classes" className="give-classes">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Ensine"/>
                         Dar Aulas
-                    </a>
+                    </Link>
                 </div>
 
                 <span className="total-connections">
